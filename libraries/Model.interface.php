c49ea34c965aa6b6cef2796b79faa3b77e53229f
Karkael Haunter <karkael@gmail.com>
2019-07-19 14:00:51 +0200

    protect request for dummies

--- libraries/Model.interface.php
+++ libraries/Model.interface.php
@@ -1,16 +1,13 @@
 <?php
-
 namespace Amonite;
 
 if( !interface_exists( "Model" ) ) {
-
 	interface Model {
-
 		static public function select( $fields = array(), $where = array(), $limit = 0, $start_at = 0 );
 		static public function selectFirst( $fields = array(), $where = array(), $start_at = 0 );
-		static public function update( $value = array(), $where = array(), $limit = 0, $start_at = 0 );
+		static public function update( $value = array(), $where = array(), $limit = 1, $start_at = 0 );
 		static public function insert( $value = array() );
 		static public function count( $where = array(), $limit = 0, $start_at = 0 );
-		static public function remove( array $where, $limit = 0, $start_at = 0 );
+		static public function remove( array $where, $limit = 1, $start_at = 0 );
 	}
 }
